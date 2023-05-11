ef37dcc380732ba350529429e3073885eb3d971b
tranbaoanhh27 <tranbaoanhh27@gmail.com>
2023-05-11 21:43:30 +0700

    Task 2

--- routes/task2Route.js
+++ routes/task2Route.js
@@ -1,8 +1,23 @@
 const express = require('express')
-const { emotions } = require('../data');
 const router = express.Router();
 
+const JARS_PERCENT = {
+    necessity: 0.55,
+    financialFreedom: 0.1,
+    give: 0.05,
+    education: 0.1,
+    longterm: 0.1,
+    play: 0.1
+};
+
 router.get('/', (request, response) => {
+    const salary = request.query.salary;
+    let jarsMoney = {...JARS_PERCENT};
+    for (let property in jarsMoney) {
+        const money = jarsMoney[property] * salary;
+        jarsMoney[property] = money.toFixed(2);
+    }
+    response.locals.jars = jarsMoney;
     response.render('task2');
 });
 
